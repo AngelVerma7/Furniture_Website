f27d03cce115cc2fda3fff9918861834edfd2a05
Shivesh Gupta <shiveshgupta2003@gmail.com>
2024-09-18 16:15:47 +0530

    added backend and seperated frontend

---
+++ backend/src/main/java/com/vishwakarmawoodworks/furniture/website/VishwakarmaWoodWorksApplication.java
@@ -0,0 +1,13 @@
+package com.vishwakarmawoodworks.furniture.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishwakarmaWoodWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishwakarmaWoodWorksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vishwakarmawoodworks/furniture/website/VishwakarmaWoodWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwakarmawoodworks.furniture.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VishwakarmaWoodWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
